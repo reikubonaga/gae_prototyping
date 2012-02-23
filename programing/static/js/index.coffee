b318fa313e5c7f4c4182068f409268e3fdaf3b8c
rei kubonaga <kubonagarei@gmail.com>
2012-02-24 06:14:31 +0900

    add 1 formula

--- programing/static/js/index.coffee
+++ programing/static/js/index.coffee
@@ -1,11 +1,143 @@
 Code ?= {}
 Code.models ?= {}
+Code.utils ?= {}
 
 class Code.models.Question extends Backbone.Model
   url:"/programing/question"
 
 class Code.models.Questions extends Backbone.Collection
   url:"/programing/questions"
+
+class Code.Ace
+  render:(id)->
+    editor = ace.edit id
+    editor.renderer.setShowGutter false
+    JavaScriptMode = require("ace/mode/javascript").Mode
+    EditSession = require("ace/edit_session").EditSession
+    lang = require "ace/lib/lang"
+    Range = require("ace/range").Range
+    editor.getSession().setMode(new JavaScriptMode())
+    return editor
+
+Code.ace = new Code.Ace
+
+class Code.Util
+  user_ans:null
+  check:(variable,code,answer)->
+    eval "Code.exe = function(){"+variable+" "+code+" return main();};"
+    @user_ans = Code.exe()
+    console.log @user_ans
+    if Number(answer) is Number(@user_ans)
+      return true
+    else
+      return false
+
+Code.util = new Code.Util
+
+#error時はundefinedとerrorログを返す
+
+Code.Math ?= {}
+
+class Code.Math.Fragment
+  x:1
+  y:1
+  set_arr:(a,b)->
+    @x = a
+    @y = b
+  set:(str)->
+    arr = str.split "/"
+    @x = arr[0]
+    @y = arr[1]
+  real:->
+    @x/@y
+  format:()->
+    if @x < 0 and @y < 0
+      return (-1*@x)+"/"+(-1*@y)
+    else if @x > 0 and @y < 0
+      return (-1*@x)+"/"+(-1*@y)
+    else if @x = 0
+      return 0
+    else if @y = 0
+      Code.Math.core.error "fragment error y=0"
+      return
+    else
+      return @x+"/"+@y
+
+class Code.Math.Formula1
+  v:0
+  x:0
+  set_arr:(a,b)->
+    @x = a
+    @v = b
+  #割り算はどうするか
+  set:(str)->
+    str = Code.Math.core.str_to_plus str
+    arr = str.split "+"
+    for xarr in arr
+      varr = xarr.split "x"
+      if varr[1] is ""
+        varr[1] = 1
+      if not varr[1]
+        if @v is 0
+          @v = Code.Math.core.str_to_number varr[0]
+        else
+          @v += Code.Math.core.str_to_number varr[0]
+      if varr[1] is 1
+        if @x is 0
+          @x = Code.Math.core.str_to_number varr[0]
+        else
+          @x += Code.Math.core.str_to_number varr[0]
+  exe:->
+    x = @x
+    v = @v
+    g = Code.Math.core.gcd(x,v)
+    f = new Code.Math.Fragment
+    f.set_arr(-v/g,x/g)
+    f.format()
+
+class Code.Math.Core
+  error:(text)->
+    console.log "error"+text
+  #最小公約数
+  gcd:(a,b)->
+    if a is 0 or b is 0
+      return undefined
+    f = true
+    r = a % b
+    while r isnt 0
+      a = b
+      b = r
+      r = a % b
+    return b
+
+  str_to_plus:(str)->
+    e_arr = str.split "="
+    if e_arr.length > 2
+      @error "equal > 2"
+      return
+    if e_arr.length = 2
+      str = e_arr[0]+"-"+e_arr[1]
+    else
+      str = e_arr[0]
+    if str.substring(0,1) is "-"
+      str= "-"+str.substring(1).replace("-","+-")
+    else
+      str=str.replace("-","+-")
+    return str
+  str_to_number:(str)->
+    if str.substring(0,1) is "-"
+      return -1*Number(str.substring 1)
+    else
+      return Number(str)
+
+  x:(str)->
+    f = new Code.Math.Formula1
+    f.set str
+    return f.exe()
+
+Code.Math.core = new Code.Math.Core
+
+$m = Code.Math.core
 
 $ ->
   class Code.PostView extends Backbone.View
@@ -24,24 +156,22 @@ $ ->
       "click .post":"post"
     check:->
       variable = @variable.getSession().getValue()
-      answer = @editor.getSession().getValue()
-      Code.answer = $("#answer_num").val()
-      eval "Code.exe = function(){"+variable+answer+"}"
-      user_ans = Code.exe()
-      if Number(Code.answer) is Number(user_ans)
-        alert("success "+user_ans)
+      code = @code.getSession().getValue()
+      answer = $("#answer_num").val()
+      if Code.util.check(variable,code,answer)
+        alert "success"+Code.util.user_ans
       else
-        alert("unsuccess "+user_ans)
+        alert "unsuccess"+Code.util.user_ans
     post:->
       variable = @variable.getSession().getValue()
-      answer = @editor.getSession().getValue()
+      code = @code.getSession().getValue()
       title = $("#title").val()
       question = $("#question").val()
-      answer_num = $("#answer_num").val()
+      answer = $("#answer").val()
       question_model = new Code.models.Question(
         title:title
-        code:answer
-        answer:answer_num
+        code:code
+        answer:answer
         question:question
         variable:variable
       )
@@ -52,24 +182,12 @@ $ ->
 
       $("#variable").css("width",$("#variable").width()+"px")
       $("#variable").css("height",$("#variable").height()+"px")
-      editor = ace.edit "variable"
-      editor.renderer.setShowGutter false
-      JavaScriptMode = require("ace/mode/javascript").Mode
-      EditSession = require("ace/edit_session").EditSession
-      lang = require "ace/lib/lang"
-      Range = require("ace/range").Range
-      editor.getSession().setMode(new JavaScriptMode())
-      @variable = editor
+      @variable = Code.ace.render "variable"
+      @variable.getSession().setValue $("#post-variable-template").html()
 
-      $("#answer").css("width",$("#answer").width()+"px")
-      #$("#answer").css("height",$("#answer").height()+"px")
-      editor = ace.edit "answer"
-      JavaScriptMode = require("ace/mode/javascript").Mode
-      EditSession = require("ace/edit_session").EditSession
-      lang = require "ace/lib/lang"
-      Range = require("ace/range").Range
-      editor.getSession().setMode(new JavaScriptMode())
-      @editor = editor
+      $("#code").css("width",$("#code").width()+"px")
+      @code = Code.ace.render "code"
+      @code.getSession().setValue $("#post-code-template").html()
 
   class Code.TopView extends Backbone.View
     initialize:->
@@ -114,21 +232,19 @@ $ ->
       "click .post":"post"
     check:->
       variable = @variable.getSession().getValue()
-      answer = @editor.getSession().getValue()
-      Code.answer = $("#answer_num").val()
-      eval "Code.exe = function(){"+variable+answer+"}"
-      user_ans = Code.exe()
-      if Number(Code.answer) is Number(user_ans)
-        alert("success "+user_ans)
+      code = @code.getSession().getValue()
+      answer = @model.get "answer"
+      if Code.util.check(variable,code,answer)
+        alert "success"+Code.util.user_ans
       else
-        alert("unsuccess "+user_ans)
+        alert "unsuccess"+Code.util.user_ans
     post:->
       variable = @variable.getSession().getValue()
-      answer = @editor.getSession().getValue()
+      code = @code.getSession().getValue()
       title = $("#title").val()
       question = $("#question").val()
       @model.set(
-        code:answer
+        code:code
         variable:variable
       )
       @model.save()
--- programing/static/js/index.js
+++ programing/static/js/index.js
@@ -1,10 +1,13 @@
 (function() {
-  var __hasProp = Object.prototype.hasOwnProperty,
+  var $m,
+    __hasProp = Object.prototype.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
   if (typeof Code === "undefined" || Code === null) Code = {};
 
   if (Code.models == null) Code.models = {};
+
+  if (Code.utils == null) Code.utils = {};
 
   Code.models.Question = (function(_super) {
 
@@ -33,6 +36,215 @@
     return Questions;
 
   })(Backbone.Collection);
+
+  Code.Ace = (function() {
+
+    function Ace() {}
+
+    Ace.prototype.render = function(id) {
+      var EditSession, JavaScriptMode, Range, editor, lang;
+      editor = ace.edit(id);
+      editor.renderer.setShowGutter(false);
+      JavaScriptMode = require("ace/mode/javascript").Mode;
+      EditSession = require("ace/edit_session").EditSession;
+      lang = require("ace/lib/lang");
+      Range = require("ace/range").Range;
+      editor.getSession().setMode(new JavaScriptMode());
+      return editor;
+    };
+
+    return Ace;
+
+  })();
+
+  Code.ace = new Code.Ace;
+
+  Code.Util = (function() {
+
+    function Util() {}
+
+    Util.prototype.user_ans = null;
+
+    Util.prototype.check = function(variable, code, answer) {
+      eval("Code.exe = function(){" + variable + " " + code + " return main();};");
+      this.user_ans = Code.exe();
+      console.log(this.user_ans);
+      if (Number(answer) === Number(this.user_ans)) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    return Util;
+
+  })();
+
+  Code.util = new Code.Util;
+
+  if (Code.Math == null) Code.Math = {};
+
+  Code.Math.Fragment = (function() {
+
+    function Fragment() {}
+
+    Fragment.prototype.x = 1;
+
+    Fragment.prototype.y = 1;
+
+    Fragment.prototype.set_arr = function(a, b) {
+      this.x = a;
+      return this.y = b;
+    };
+
+    Fragment.prototype.set = function(str) {
+      var arr;
+      arr = str.split("/");
+      this.x = arr[0];
+      return this.y = arr[1];
+    };
+
+    Fragment.prototype.real = function() {
+      return this.x / this.y;
+    };
+
+    Fragment.prototype.format = function() {
+      if (this.x < 0 && this.y < 0) {
+        return (-1 * this.x) + "/" + (-1 * this.y);
+      } else if (this.x > 0 && this.y < 0) {
+        return (-1 * this.x) + "/" + (-1 * this.y);
+      } else if (this.x = 0) {
+        return 0;
+      } else if (this.y = 0) {
+        Code.Math.core.error("fragment error y=0");
+      } else {
+        return this.x + "/" + this.y;
+      }
+    };
+
+    return Fragment;
+
+  })();
+
+  Code.Math.Formula1 = (function() {
+
+    function Formula1() {}
+
+    Formula1.prototype.v = 0;
+
+    Formula1.prototype.x = 0;
+
+    Formula1.prototype.set_arr = function(a, b) {
+      this.x = a;
+      return this.v = b;
+    };
+
+    Formula1.prototype.set = function(str) {
+      var arr, varr, xarr, _i, _len, _results;
+      str = Code.Math.core.str_to_plus(str);
+      arr = str.split("+");
+      _results = [];
+      for (_i = 0, _len = arr.length; _i < _len; _i++) {
+        xarr = arr[_i];
+        varr = xarr.split("x");
+        if (varr[1] === "") varr[1] = 1;
+        if (!varr[1]) {
+          if (this.v === 0) {
+            this.v = Code.Math.core.str_to_number(varr[0]);
+          } else {
+            this.v += Code.Math.core.str_to_number(varr[0]);
+          }
+        }
+        if (varr[1] === 1) {
+          if (this.x === 0) {
+            _results.push(this.x = Code.Math.core.str_to_number(varr[0]));
+          } else {
+            _results.push(this.x += Code.Math.core.str_to_number(varr[0]));
+          }
+        } else {
+          _results.push(void 0);
+        }
+      }
+      return _results;
+    };
+
+    Formula1.prototype.exe = function() {
+      var f, g, v, x;
+      x = this.x;
+      v = this.v;
+      g = Code.Math.core.gcd(x, v);
+      f = new Code.Math.Fragment;
+      f.set_arr(-v / g, x / g);
+      return f.format();
+    };
+
+    return Formula1;
+
+  })();
+
+  Code.Math.Core = (function() {
+
+    function Core() {}
+
+    Core.prototype.error = function(text) {
+      return console.log("error" + text);
+    };
+
+    Core.prototype.gcd = function(a, b) {
+      var f, r;
+      if (a === 0 || b === 0) return;
+      f = true;
+      r = a % b;
+      while (r !== 0) {
+        a = b;
+        b = r;
+        r = a % b;
+      }
+      return b;
+    };
+
+    Core.prototype.str_to_plus = function(str) {
+      var e_arr;
+      e_arr = str.split("=");
+      if (e_arr.length > 2) {
+        this.error("equal > 2");
+        return;
+      }
+      if (e_arr.length = 2) {
+        str = e_arr[0] + "-" + e_arr[1];
+      } else {
+        str = e_arr[0];
+      }
+      if (str.substring(0, 1) === "-") {
+        str = "-" + str.substring(1).replace("-", "+-");
+      } else {
+        str = str.replace("-", "+-");
+      }
+      return str;
+    };
+
+    Core.prototype.str_to_number = function(str) {
+      if (str.substring(0, 1) === "-") {
+        return -1 * Number(str.substring(1));
+      } else {
+        return Number(str);
+      }
+    };
+
+    Core.prototype.x = function(str) {
+      var f;
+      f = new Code.Math.Formula1;
+      f.set(str);
+      return f.exe();
+    };
+
+    return Core;
+
+  })();
+
+  Code.Math.core = new Code.Math.Core;
+
+  $m = Code.Math.core;
 
   $(function() {
     var route;
@@ -64,30 +276,28 @@
       };
 
       PostView.prototype.check = function() {
-        var answer, user_ans, variable;
+        var answer, code, variable;
         variable = this.variable.getSession().getValue();
-        answer = this.editor.getSession().getValue();
-        Code.answer = $("#answer_num").val();
-        eval("Code.exe = function(){" + variable + answer + "}");
-        user_ans = Code.exe();
-        if (Number(Code.answer) === Number(user_ans)) {
-          return alert("success " + user_ans);
+        code = this.code.getSession().getValue();
+        answer = $("#answer_num").val();
+        if (Code.util.check(variable, code, answer)) {
+          return alert("success" + Code.util.user_ans);
         } else {
-          return alert("unsuccess " + user_ans);
+          return alert("unsuccess" + Code.util.user_ans);
         }
       };
 
       PostView.prototype.post = function() {
-        var answer, answer_num, question, question_model, title, variable;
+        var answer, code, question, question_model, title, variable;
         variable = this.variable.getSession().getValue();
-        answer = this.editor.getSession().getValue();
+        code = this.code.getSession().getValue();
         title = $("#title").val();
         question = $("#question").val();
-        answer_num = $("#answer_num").val();
+        answer = $("#answer").val();
         question_model = new Code.models.Question({
           title: title,
-          code: answer,
-          answer: answer_num,
+          code: code,
+          answer: answer,
           question: question,
           variable: variable
         });
@@ -95,26 +305,14 @@
       };
 
       PostView.prototype.render = function() {
-        var EditSession, JavaScriptMode, Range, editor, lang;
         this.$el.append(this.template());
         $("#variable").css("width", $("#variable").width() + "px");
         $("#variable").css("height", $("#variable").height() + "px");
-        editor = ace.edit("variable");
-        editor.renderer.setShowGutter(false);
-        JavaScriptMode = require("ace/mode/javascript").Mode;
-        EditSession = require("ace/edit_session").EditSession;
-        lang = require("ace/lib/lang");
-        Range = require("ace/range").Range;
-        editor.getSession().setMode(new JavaScriptMode());
-        this.variable = editor;
-        $("#answer").css("width", $("#answer").width() + "px");
-        editor = ace.edit("answer");
-        JavaScriptMode = require("ace/mode/javascript").Mode;
-        EditSession = require("ace/edit_session").EditSession;
-        lang = require("ace/lib/lang");
-        Range = require("ace/range").Range;
-        editor.getSession().setMode(new JavaScriptMode());
-        return this.editor = editor;
+        this.variable = Code.ace.render("variable");
+        this.variable.getSession().setValue($("#post-variable-template").html());
+        $("#code").css("width", $("#code").width() + "px");
+        this.code = Code.ace.render("code");
+        return this.code.getSession().setValue($("#post-code-template").html());
       };
 
       return PostView;
@@ -214,27 +412,25 @@
       };
 
       TryView.prototype.check = function() {
-        var answer, user_ans, variable;
+        var answer, code, variable;
         variable = this.variable.getSession().getValue();
-        answer = this.editor.getSession().getValue();
-        Code.answer = $("#answer_num").val();
-        eval("Code.exe = function(){" + variable + answer + "}");
-        user_ans = Code.exe();
-        if (Number(Code.answer) === Number(user_ans)) {
-          return alert("success " + user_ans);
+        code = this.code.getSession().getValue();
+        answer = this.model.get("answer");
+        if (Code.util.check(variable, code, answer)) {
+          return alert("success" + Code.util.user_ans);
         } else {
-          return alert("unsuccess " + user_ans);
+          return alert("unsuccess" + Code.util.user_ans);
         }
       };
 
       TryView.prototype.post = function() {
-        var answer, question, title, variable;
+        var code, question, title, variable;
         variable = this.variable.getSession().getValue();
-        answer = this.editor.getSession().getValue();
+        code = this.code.getSession().getValue();
         title = $("#title").val();
         question = $("#question").val();
         this.model.set({
-          code: answer,
+          code: code,
           variable: variable
         });
         return this.model.save();
